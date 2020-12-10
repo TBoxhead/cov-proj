8ab711123b8cb901891a998ecf585bc9b04c51dd
Cristian Tocut <andrei.tocut1@gmail.com>
2020-12-10 15:53:08 +0200

    Added react native paper

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world! </Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
